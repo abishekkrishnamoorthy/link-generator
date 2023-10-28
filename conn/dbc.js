2e33512af5d7d40ec9e40cca1a57bc76c82ea143
Abishek krishnamoorthy <102430868+abishekkrishnamoorthy@users.noreply.github.com>
2023-10-28 19:55:22 +0530

    version 1.0

---
+++ conn/dbc.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const express= require('express')
+const router=express.Router()
+const path=require('path')
+const fs=require('fs')
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database:"link"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+module.exports=con
--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const express=require('express')
 const app=express()
 const path=require('path')
-
+const con = require('./conn/dbc')
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(express.static(path.join('./public')))
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'views','index.html'))
 }).listen(3000,()=>{ console.log('running')})
+
+app.post('/created',(req,res)=>{
+    const before=req.body.linkbefore
+    const after=`/${req.body.linkafter}`
+    console.log(after)
+    var sql = "INSERT INTO `map`(`link`,`short`) VALUES ('"+before+"','"+after+"')"
+    con.query(sql,function (err, result) {
+        if (err) throw err;
+        console.log("inserted");
+        res.send("sucess")
+      });
+    
+})
+app.get('^/([^/]+)$',(req,res)=>{
+    const url=req.url
+    var sql="SELECT * FROM `map` WHERE `short`='"+url+"'"
+    con.query(sql,(error, results, fields)=>{
+        const ev=JSON.stringify(results)
+        const data=JSON.parse(ev)
+        if(results.length>0){
+            if(data[0].short==url)
+            res.redirect(data[0].link)
+        }
+        else{
+            res.send("not found error")
+        }
+})
+})
